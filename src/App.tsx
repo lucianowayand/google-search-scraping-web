a8e35a86bf4ef0371dd3e3c227715199cc360931
Luciano <36968613+lucianowayand@users.noreply.github.com>
2023-11-30 07:51:32 -0300

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Google Search Scraping Web
+      </h1>
+      <Test />
+    </div>
+  );
+}
